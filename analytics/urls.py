c1d56e6d319fd54e16e40e9310e5c08e881fc25f
kburts <kevin.burton2@gmail.com>
2018-02-10 12:53:12 -0800

    Clean up frontend templates.

--- analytics/urls.py
+++ analytics/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from .views import home
+from .views import home, ProfileRedirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/profile/$', ProfileRedirect.as_view()),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^v1/events/', include('events.api_urls', namespace='events_api')),
 
--- analytics/views.py
+++ analytics/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import RedirectView
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+class ProfileRedirect(RedirectView):
+    pattern_name = 'home'
